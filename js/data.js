bc75d03715408241544a1f9cab56d4db14f0ed9d
jackshawn <jackshawn@live.com>
2016-11-25 17:17:01 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,222 +1,551 @@
-var movie = [
-    {
-        name:'xxx',
-        star:2,
-        date:'2015-11-11',
-        image:'http://subhd.com/images/back/doctorstrange.jpg',
-        // image:'images/2.jpg',
-        remark:'qweqwe'
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-12',
-        image:'images/2.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3.6,
-        date:'2015-11-13',
-        image:'images/3.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3.1,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-11',
-        image:'images/1.jpg',
-        remark:'qweqwe'
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-12',
-        image:'images/2.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-13',
-        image:'images/3.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-14',
-        image:'images/4.jpg',
-        remark:''
-    },
-    {
-        name:'xxx',
-        star:3,
-        date:'2015-11-15',
-        image:'images/5.jpg',
-        remark:''
+var movie = {
+    all: [
+        {
+            name: 'War Dogs',
+            star: 3.8,
+            date: '2016-11-22',
+            image: '',
+            remark: 'mode in china被黑的最惨的一次'
+        },
+        {
+            name: 'Mechanic: Resurrection',
+            star: 1.5,
+            date: '2016-11-13',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Hell or High Water',
+            star: 3.9,
+            date: '2016-11-20',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Secret Life of Pets',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Finding Dory',
+            star: 3.4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '追凶者也',
+            star: 4,
+            date: '',
+            image: '',
+            remark: '张译演的真好'
+        },
+        {
+            name: 'Sausage Party',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: '最爱诺顿竟然是个娘娘腔的甜甜圈'
+        },
+        {
+            name: '寒战2',
+            star: 3.4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '神奇队长',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: '拖家带口版的《荒野生存》'
+        },
+        {
+            name: 'Nerve',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: '解释游戏那段视频挺魔性的'
+        },
+        {
+            name: 'Swiss Army Man',
+            star: 4.1,
+            date: '',
+            image: '',
+            remark: '死尸活成了我，我似乎从来就没有活过。深刻，沉重'
+        },
+        {
+            name: 'X-Men: Apocalypse',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '三人行',
+            star: 3.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Shallows',
+            star: 3.4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Captain America: Civil War',
+            star: 4,
+            date: '',
+            image: '',
+            remark: '嗯，机场那段很燃'
+        },
+        {
+            name: 'The Nice Guys',
+            star: 3,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '老炮儿',
+            star: 4,
+            date: '',
+            image: '',
+            remark: '开始很期待两帮人撕起来，后来发现高潮就是六爷冰场步履艰辛那段。BGM鼓点有力，冯导演技精湛。'
+        },
+        {
+            name: 'Criminal',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Sing Stree',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: 'BGM很赞'
+        },
+        {
+            name: ' Batman v Superman: Dawn of Justice',
+            star: 3.4,
+            date: '',
+            image: '',
+            remark: '只看名字，一股奥特曼大战孙悟空的山寨感'
+        },
+        {
+            name: '树大招风',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '老笠',
+            star: 3.7,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '10 Cloverfield Lane',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '美人鱼',
+            star: 3.7,
+            date: '',
+            image: '',
+            remark: '星爷电影还是功夫最好看'
+        },
+        {
+            name: 'The Brothers Grimsby',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Zootopia',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '夏洛特烦恼 ',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Deadpool',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: '很贫'
+        },
+        {
+            name: 'The Revenant',
+            star: 4.3,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Elle',
+            star: 2,
+            date: '',
+            image: '',
+            remark: '有钱人吃饱了撑的'
+        },
+        {
+            name: 'Train to Busan ',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: '韩国的丧尸片，可以的'
+        },
+        {
+            name: 'En man som heter Ove',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Crew',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Demolition',
+            star: 4.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Eye in the Sky',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Good Dinosaur',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Dirty Grandpa',
+            star: 3.2,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '踏血寻梅',
+            star: 3.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '我不是王毛',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '叶问3',
+            star: 3.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Ted 2',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '唐人街探案',
+            star: 3.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '寻龙诀',
+            star: 3.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '我是路人甲',
+            star: 3.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Black Mass',
+            star: 3,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Martian',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Big Hero 6',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '解救吾先生',
+            star: 3.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '边境杀手',
+            star: 0.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '绝命海拔',
+            star: 0.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Transformers: Age of Extinction',
+            star: 2.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Wolf of Wall Street',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Ant-Man',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '暗杀',
+            star: 0.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Chappie',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Mission: Impossible - Rogue Nation',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Youth',
+            star: 0.1,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Man from U.N.C.L.E.',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'The Gift',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Fantastic Four',
+            star: 2,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '烈日灼心',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Minions',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Inside Out',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Pixels',
+            star: 2.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Jurassic World',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '杀破狼2',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Dope',
+            star: 3.8,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'San Andreas',
+            star: 3,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Terminator Genisys',
+            star: 3.6,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: '明日世界',
+            star: 3,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Avengers: Age of Ultron',
+            star: 3.5,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Cop Car',
+            star: 3.7,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Ex Machina',
+            star: 3.9,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Mad Max: Fury Road',
+            star: 4.1,
+            date: '',
+            image: '',
+            remark: '燃'
+        },
+        {
+            name: 'X-Men: Days of Future Past',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Home',
+            star: 3,
+            date: '',
+            image: '',
+            remark: ''
+        },
+        {
+            name: 'Kingsman: The Secret Service',
+            star: 4,
+            date: '',
+            image: '',
+            remark: ''
+        }
+    ],
+    likes: {
+        movies: [],
+        derectors: [],
+        actors: [],
+        games: [],
+        songs: [],
+        books: []
     }
-
-];
+}
