d6e8e2d2d1e35164dfcb4552dc0800b76f036635
jackshawn <jackshawn@live.com>
2016-12-28 17:06:54 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,47 +4,55 @@ var movies = {
             name: 'Nocturnal.Animals',
             star: 3.7,
             date: '2016-12-25',
+            image: 'http://p0.meituan.net/movie/ea8a1b97b3fe07e5cb30f5cfef28931c67217.jpg',
             remark: ''
         },
         {
             name: 'Inferno',
             star: 2.8,
             date: '2016-12-24',
+            image: 'http://p0.meituan.net/movie/8c5dcfb772215c306776a03f48c6e34d421433.jpg',
             remark: '除了女主还有啥看的'
         },
         {
             name: 'The Accountant',
             star: 3.6,
             date: '2016-12-20',
+            image: 'http://p0.meituan.net/mmdb/09660d76cf7b4c05b0f762e4926f2a95150063.jpg',
             remark: ''
         },
         {
             name: 'Spectral',
             star: 3.6,
             date: '2016-12-18',
+            image: 'http://p1.meituan.net/movie/2bf27269fd27499708194d902fbd9ad279645.jpg',
             remark: ''
         },
         {
             name: '驴得水',
             star: 3,
             date: '2016-12-14',
+            image: 'http://p1.meituan.net/mmdb/13d4883803f25244d266c7cd3ac2f15d327618.jpg',
             remark: '所现在拍电影只要有人就就行了么'
         },
         {
             name: 'The Magnificent Seven',
             star: 3,
+            image: 'http://p1.meituan.net/mmdb/41264a3c94e4665ed4586e4642240488124117.jpg',
             date: '2016-12-11',
             remark: ''
         },
         {
             name: 'Sully',
             star: 3.8,
+            image: 'http://p0.meituan.net/mmdb/1146b99f8f05f155573919e5265f1b77216042.jpg',
             date: '2016-12-3',
             remark: ''
         },
         {
             name: 'Snowden',
             star: 3.6,
+            image: 'http://p1.meituan.net/mmdb/295019796c3a51d336546a134dae0677144569.jpg',
             date: '2016-11-23',
             remark: ''
         },
@@ -52,462 +60,539 @@ var movies = {
             name: 'War Dogs',
             star: 3.8,
             date: '2016-11-22',
-            remark: 'mode in china被黑的最惨的一次'
+            image: 'http://p1.meituan.net/mmdb/7b6ee0e1f58a915cb37bf6e833d27f51581632.jpg',
+            remark: 'made in china被黑的最惨的一次'
         },
         {
             name: 'Mechanic: Resurrection',
             star: 1.5,
             date: '2016-11-13',
-            remark: ''
+            image: 'http://p0.meituan.net/movie/41b1b9f63a0affb3c2ce9ac849767c1f1410759.jpg',
+            remark: '卖卖肉，打一打，实在没有看点'
         },
         {
             name: 'Hell or High Water',
             star: 3.9,
             date: '2016-11-20',
+            image: 'http://p0.meituan.net/movie/642862b082663f5c4628306d84d34825113202.jpg',
             remark: ''
         },
         {
             name: 'The Secret Life of Pets',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Finding Dory',
             star: 3.4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '追凶者也',
             star: 4,
             date: '',
+            image: '',
             remark: '张译演的真好'
         },
         {
             name: 'Sausage Party',
             star: 3.8,
             date: '',
+            image: '',
             remark: '最爱诺顿竟然是个娘娘腔的甜甜圈'
         },
         {
             name: '寒战2',
             star: 3.4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '神奇队长',
             star: 3.8,
             date: '',
+            image: '',
             remark: '拖家带口版的《荒野生存》'
         },
         {
             name: 'Nerve',
             star: 3.8,
             date: '',
+            image: '',
             remark: '解释游戏那段视频挺魔性的'
         },
         {
             name: 'Swiss Army Man',
             star: 4.1,
             date: '',
+            image: '',
             remark: '死尸活成了我，我似乎从来就没有活过。深刻，沉重'
         },
         {
             name: 'X-Men: Apocalypse',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '三人行',
             star: 3.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Shallows',
             star: 3.4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Captain America: Civil War',
             star: 4,
             date: '',
+            image: '',
             remark: '嗯，机场那段很燃'
         },
         {
             name: 'The Nice Guys',
             star: 3,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '老炮儿',
             star: 4,
             date: '',
+            image: '',
             remark: '开始很期待两帮人撕起来，后来发现高潮就是六爷冰场步履艰辛那段。BGM鼓点有力，冯导演技精湛。'
         },
         {
             name: 'Criminal',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Sing Stree',
             star: 3.9,
             date: '',
+            image: '',
             remark: 'BGM很赞'
         },
         {
             name: ' Batman v Superman: Dawn of Justice',
             star: 3.4,
             date: '',
+            image: '',
             remark: '只看名字，一股奥特曼大战孙悟空的山寨感'
         },
         {
             name: '树大招风',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '老笠',
             star: 3.7,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '10 Cloverfield Lane',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '美人鱼',
             star: 3.7,
             date: '',
+            image: '',
             remark: '星爷电影还是功夫最好看'
         },
         {
             name: 'The Brothers Grimsby',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Zootopia',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '夏洛特烦恼 ',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Deadpool',
             star: 3.9,
             date: '',
+            image: '',
             remark: '很贫'
         },
         {
             name: 'The Revenant',
             star: 4.3,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Elle',
             star: 2,
             date: '',
+            image: '',
             remark: '有钱人吃饱了撑的'
         },
         {
             name: 'Train to Busan ',
             star: 3.8,
             date: '',
+            image: '',
             remark: '韩国的丧尸片，可以的'
         },
         {
             name: 'En man som heter Ove',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Crew',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Demolition',
             star: 4.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Eye in the Sky',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Good Dinosaur',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Dirty Grandpa',
             star: 3.2,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '踏血寻梅',
             star: 3.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '我不是王毛',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '叶问3',
             star: 3.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Ted 2',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '唐人街探案',
             star: 3.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '寻龙诀',
             star: 3.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '我是路人甲',
             star: 3.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Black Mass',
             star: 3,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Martian',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Big Hero 6',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '解救吾先生',
             star: 3.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '边境杀手',
             star: 0.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '绝命海拔',
             star: 0.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Transformers: Age of Extinction',
             star: 2.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Wolf of Wall Street',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Ant-Man',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '暗杀',
             star: 0.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Chappie',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Mission: Impossible - Rogue Nation',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Youth',
             star: 0.1,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Man from U.N.C.L.E.',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'The Gift',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Fantastic Four',
             star: 2,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '烈日灼心',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Minions',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Inside Out',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Pixels',
             star: 2.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Jurassic World',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '杀破狼2',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Dope',
             star: 3.8,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'San Andreas',
             star: 3,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Terminator Genisys',
             star: 3.6,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: '明日世界',
             star: 3,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Avengers: Age of Ultron',
             star: 3.5,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Cop Car',
             star: 3.7,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Ex Machina',
             star: 3.9,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Mad Max: Fury Road',
             star: 4.1,
             date: '',
+            image: '',
             remark: '燃'
         },
         {
             name: 'X-Men: Days of Future Past',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Home',
             star: 3,
             date: '',
+            image: '',
             remark: ''
         },
         {
             name: 'Kingsman: The Secret Service',
             star: 4,
             date: '',
+            image: '',
             remark: ''
         }
     ],
